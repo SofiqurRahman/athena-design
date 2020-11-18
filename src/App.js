fe10720a0eb42fe152a14b20f559552136701109
Sofiqur Rahman <sofiqurr849@gmail.com>
2020-11-19 03:40:19 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import DedicatedTeam from './components/DedicatedTeam/DedicatedTeam';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <DedicatedTeam />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
